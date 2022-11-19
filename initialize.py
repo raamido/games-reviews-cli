8973ea77b210db41e927eb0c7d17c46ab705feb7
Rami <ramiabrahem24@gmail.com>
2022-11-20 00:37:01 +0200

    initialize the database

---
+++ initialize.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:///reviews.sqlite', echo=False)
+
+session = sessionmaker(bind=engine)()
+
+Base = declarative_base()
+
+
+class Review(Base):
+    __tablename__ = "Reviews"
+
+    id = Column(Integer, primary_key=True)
+    game_name = Column(String(50))
+    rate = Column(Integer)
+    review = Column(String(200))
+
+
+Base.metadata.create_all(engine)
